0393f6328130a43dc6a6151ccc38c2baffe9dd4d
ARUNKARTHICK782 <arunkarthick.cs20@bitsathy.ac.in>
2023-02-18 11:55:04 +0530

    Testing Pages

--- Client/lib/main.dart
+++ Client/lib/main.dart
@@ -30,7 +30,7 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return LoginPage();
+    return AdminHomePage();
   }
 }
 
