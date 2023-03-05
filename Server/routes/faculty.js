55ea200cc9b446661456d31ac47cc7e1a5bc5a7e
Keerthi Vibisan <trimaqua7760@gmail.com>
2023-03-05 19:02:04 +0530

    🧑‍🎓  Getting all requested students under a Faculty API added

--- Server/routes/faculty.js
+++ Server/routes/faculty.js
@@ -12,7 +12,31 @@ route.get("/getStudents/:fid", authenticate, (req, res) => {
     //! 🫣🫣🫣 Faculty id must be taken dynamically from DB ⚠️⚠️⚠️⚠️⚠️⚠️⚠️ 
     let f_id = req.params.fid;
     let q = `select * from STUDENT where FACULTY_ID="${f_id}"`;
-    console.log(q);
+    
+    try {
+        sql_con.query(q, (err, result) => {
+            if(err) {
+                console.log("Error ---> ", err);
+                res.send("Error").status(500);
+            }
+            else {
+                res.json(result).status(200);
+            }
+        })
+    }
+    catch(err) {
+        console.log("Something error ---> ", err);
+        res.send("Error").status(500);
+    }
+
+})
+
+//! Students REQUESTEDTO TEACHER TO CHANGE THE LAB
+route.get("/getReqStudents/:fid", authenticate, (req, res) => {
+    //! 💀💀 Faculty id must be taken dynamically from DB ⚠️ 
+    let f_id = req.params.fid;
+    let q = `SELECT * FROM REQUESTS WHERE FROM_LAB_FAC_ID ="${f_id}" OR TO_LAB_FAC_ID ="${f_id}";`;
+  
     try {
         sql_con.query(q, (err, result) => {
             if(err) {
