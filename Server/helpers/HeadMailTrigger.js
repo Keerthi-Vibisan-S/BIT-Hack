e23d828b0492b0329e2a65b76a55897cf46ec49e
Keerthi Vibisan (LAB_Server) <keerthivibisan.ct20@bitsathy.ac.in>
2023-03-30 10:47:01 +0530

    ✅ From lab faculty approval and Special Labs Head Mail triggers added'

---
+++ Server/helpers/HeadMailTrigger.js
@@ -0,0 +1,26 @@
+const sql_con = require('../settings/databaseConnection'); 
+const sendEmail = require('../templates/mailTemplate');
+
+function checkApprovals(r_id) {
+    
+    try {
+        const q = `SELECT * FROM REQUESTS WHERE R_ID = ${r_id} AND FROM_APPROVAL = "OK" AND TO_APPROVAL="OK";`;
+        return new  Promise((resolve, reject) => {
+            sql_con.query(q, (err, result) => {
+            if(err) {
+                console.log(err);
+                reject("Error");
+            }
+            else {
+                if(result.length == 1) sendEmail("keerthivibisan2211@gmail.com", "Both Labs", "Approved");
+                resolve("Done");
+            }
+        })})
+    }
+    catch(err){
+        console.log("An error ------> "+err);
+        return("Error");
+    }
+}
+
+module.exports = checkApprovals;
--- Server/routes/requests.js
+++ Server/routes/requests.js
@@ -2,6 +2,9 @@ const express = require("express");
 const sql_con = require("../settings/databaseConnection");
 const authenticate = require("../helpers/auth_middleware");
 const findUser = require("../helpers/findUser");
+const con = require("../settings/databaseConnection");
+const sendEmail = require("../templates/mailTemplate");
+const checkApprovals = require("../helpers/HeadMailTrigger");
 
 const route = express.Router();
 
@@ -35,5 +38,41 @@ route.post("/addReq", authenticate, async (req, res) => {
     }
   });
 });
+
+//! FROM LAB FACULTY APPROVAL
+//! 💀 Faculty id must be taken from mail authorization
+route.patch("/fromDecision/:id", (req, res) => {
+  let f_id = req.params.id; // This must be dynamic
+
+  let s_id = req.body.stu_id;
+  let r_id = req.body.r_id;
+  let decision = req.body.decision;
+  let q = `UPDATE REQUESTS SET FROM_APPROVAL = "OK" WHERE FROM_LAB_FAC_ID = "${f_id}" AND R_ID = "${r_id}";`;
+  let stu_q = `SELECT STU_EMAIL FROM STUDENT where STU_ID = "${s_id}";`;
+  try {
+    sql_con.query(`${q}${stu_q}`, (err, result) => {
+      if(err) {
+        console.log("An error ---> ", err);
+        res.send("Server side error").status(500);
+      }
+      else {
+        // 1. Trigger mail send function
+        // 2. Response to frontend
+        // console.log(result[0]);
+        //console.log(result[1][0].STU_EMAIL);
+        //! Sending email to Student
+        sendEmail(result[1][0].STU_EMAIL, "From lab Approval", decision);
+        res.send("Your Decision Updated").status(200);
+
+        //! Wen need to check for both approval so we can send mail to HEAD
+        checkApprovals(r_id);
+      }
+    })
+  } catch (error) {
+    console.log(error);
+    res.send("Server side error").status(500);
+  }
+
+})
 
 module.exports = route;
